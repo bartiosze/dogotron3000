90159e6935415e2ca2870fd0cb5c2defba080600
Bartosz Kaliszuk <bartosz.kaliszuk@gmail.com>
2015-03-15 17:43:18 +0100

    Update build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+var plugins = require('gulp-load-plugins')();
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('bower', function () {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({}))
+    .pipe(gulp.dest('./dest'));
 });
