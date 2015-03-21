40e3b1f42baf77db2ae76f632e5be81a9ea0e281
Bartosz Kaliszuk <bartosz.kaliszuk@gmail.com>
2015-03-21 22:00:14 +0100

    Add gh-pages files preparation

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gulp = require('gulp'),
     wiredep = require('wiredep').stream,
     plugins = require('gulp-load-plugins')();
 
-gulp.task('default', function() {
-});
+var target = gulp.src('./html/index.html');
+var vendorFiles = gulp.src(['./build/vendor.js', './build/vendor.css']);
+gulp.task('default', ['pages']);
 
 gulp.task('index', function () {
-  var target = gulp.src('./html/index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths:
   var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], {read: false});
 
@@ -22,4 +22,20 @@ gulp.task('index', function () {
 // 2. concat vendor scripts
 // 3. inject vendor scripts
 gulp.task('pages', function () {
-})
+  // first copy bower main files to build directory
+  gulp.src(bowerFiles())
+    .pipe(plugins.copy('build/vendors', {prefix: 2}));
+  // now handle *.js files
+  gulp.src('build/vendors/**/*.js')
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('vendor.js'))
+    .pipe(gulp.dest('./build'));
+  // do the same for css
+  gulp.src('build/vendors/**/*.css')
+    .pipe(plugins.minifyCss())
+    .pipe(plugins.concat('vendor.css'))
+    .pipe(gulp.dest('./build'));
+  target
+    .pipe(plugins.inject(vendorFiles, {name: 'bower'}))
+    .pipe(gulp.dest('./build'));
+});
